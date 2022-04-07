dff73f4dcb65b2697b571cbfbcc1535935e131fa
MinZombie <78908490+MinZombie@users.noreply.github.com>
2022-04-07 17:00:02 +0900

    [기능추가] APIManager 함수 및 테스트코드 추가
    - url 주소를 반환하는 함수 구현

--- PhotosMVVM/Service/APIManager.swift
+++ PhotosMVVM/Service/APIManager.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+enum Endpoint: String {
+    case search = "search/photos"
+}
+
+final class APIManager {
+    func url(endpoint: Endpoint, params: [String: String]) -> URL? {
+        var baseUrl = "https://api.unsplash.com/" + endpoint.rawValue + "?" 
+        
+        var queryItems = [URLQueryItem]()
+        
+        for (name, value) in params {
+            queryItems.append(.init(name: name, value: value))
+        }
+        
+        queryItems.append(.init(name: "client_id", value: Bundle.main.apiKey))
+        
+        baseUrl += queryItems.map { "\($0.name)=\($0.value ?? "")" }.joined(separator: "&")
+        
+        return URL(string: baseUrl)
+    }
+}
--- PhotosMVVMTests/PhotosMVVMTests.swift
+++ PhotosMVVMTests/PhotosMVVMTests.swift
@@ -6,29 +6,30 @@
 //
 
 import XCTest
+@testable import PhotosMVVM
 
 class PhotosMVVMTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var service: APIManager!
+    
+    override func setUp() {
+        service = APIManager()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    func test_url주소가_제대로_반환되는지() {
+        
+        let result = service.url(
+            endpoint: .search,
+            params: [
+                "page" : "1",
+                "query": "canada"
+            ]
+        )
+        
+        if let result = result {
+            XCTAssertTrue(result.absoluteString.contains("canada"))
+        } else {
+            XCTFail()
         }
     }
 
